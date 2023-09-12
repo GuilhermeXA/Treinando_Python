720002bcb9bd4f1708e753f18cd8ed870236c41e
Guilherme Assunção <82071553+GuilhermeXA@users.noreply.github.com>
2023-09-11 22:44:15 -0300

    Modificando o projeto, corrigindo erros

--- Treinando Python 11-09-2023_Sistema Loja usando pysimplegui e sql server/App.py
+++ Treinando Python 11-09-2023_Sistema Loja usando pysimplegui e sql server/App.py
@@ -1,124 +1,72 @@
 import PySimpleGUI as sg
-import sqlite3
+import pyodbc
 
-# Função para criar a tabela de produtos no banco de dados
-def criar_tabela():
-    conn = sqlite3.connect('loja.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS produtos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
-            marca TEXT NOT NULL,
-            preco REAL NOT NULL,
-            quantidade INTEGER NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+# Conecte-se ao banco de dados SQL Server
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=seu_servidor;'
+                      'Database=LojaDB;'
+                      'Trusted_Connection=yes;')
 
-# Função para inserir um novo produto no banco de dados
-def cadastrar_produto(nome, marca, preco, quantidade):
-    conn = sqlite3.connect('loja.db')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO produtos (nome, marca, preco, quantidade) VALUES (?, ?, ?, ?)',
-                   (nome, marca, preco, quantidade))
-    conn.commit()
-    conn.close()
+cursor = conn.cursor()
 
-# Função para listar todos os produtos cadastrados
-def listar_produtos():
-    conn = sqlite3.connect('loja.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM produtos')
-    produtos = cursor.fetchall()
-    conn.close()
-    return produtos
-
-# Função para atualizar um produto no banco de dados
-def atualizar_produto(id, nome, marca, preco, quantidade):
-    conn = sqlite3.connect('loja.db')
-    cursor = conn.cursor()
-    cursor.execute('UPDATE produtos SET nome=?, marca=?, preco=?, quantidade=? WHERE id=?',
-                   (nome, marca, preco, quantidade, id))
-    conn.commit()
-    conn.close()
-
-# Função para excluir um produto do banco de dados
-def excluir_produto(id):
-    conn = sqlite3.connect('loja.db')
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM produtos WHERE id=?', (id,))
-    conn.commit()
-    conn.close()
-
-# Layout da janela
+# Layout da interface
 layout = [
+    [sg.Text('ID:'), sg.InputText(key='id')],
     [sg.Text('Nome:'), sg.InputText(key='nome')],
     [sg.Text('Marca:'), sg.InputText(key='marca')],
     [sg.Text('Preço:'), sg.InputText(key='preco')],
     [sg.Text('Quantidade:'), sg.InputText(key='quantidade')],
     [sg.Button('Cadastrar'), sg.Button('Editar'), sg.Button('Excluir')],
-    [sg.Listbox(values=[], size=(50, 10), key='produtos')],
+    [sg.Table(values=[], headings=['ID', 'Nome', 'Marca', 'Preço', 'Quantidade'], auto_size_columns=False, justification='right', num_rows=10, key='table')]
 ]
 
-# Janela principal
-janela = sg.Window('Loja').Layout(layout)
+window = sg.Window('Loja', layout, finalize=True)
 
-# Criar a tabela no banco de dados
-criar_tabela()
+# Função para limpar campos de entrada
+def limpar_campos():
+    window['id'].update('')
+    window['nome'].update('')
+    window['marca'].update('')
+    window['preco'].update('')
+    window['quantidade'].update('')
 
+# Função para atualizar a tabela
+def atualizar_tabela():
+    cursor.execute('SELECT * FROM Produtos')
+    produtos = cursor.fetchall()
+    window['table'].update(values=produtos)
+
+# Loop principal da interface
 while True:
-    evento, valores = janela.Read()
+    event, values = window.read()
 
-    if evento == sg.WINDOW_CLOSED:
+    if event == sg.WINDOW_CLOSED:
         break
+    elif event == 'Cadastrar':
+        nome = values['nome']
+        marca = values['marca']
+        preco = values['preco']
+        quantidade = values['quantidade']
+        cursor.execute('INSERT INTO Produtos (Nome, Marca, Preco, Quantidade) VALUES (?, ?, ?, ?)', (nome, marca, preco, quantidade))
+        conn.commit()
+        limpar_campos()
+        atualizar_tabela()
+    elif event == 'Editar':
+        id_produto = values['id']
+        nome = values['nome']
+        marca = values['marca']
+        preco = values['preco']
+        quantidade = values['quantidade']
+        cursor.execute('UPDATE Produtos SET Nome = ?, Marca = ?, Preco = ?, Quantidade = ? WHERE ID = ?', (nome, marca, preco, quantidade, id_produto))
+        conn.commit()
+        limpar_campos()
+        atualizar_tabela()
+    elif event == 'Excluir':
+        id_produto = values['id']
+        cursor.execute('DELETE FROM Produtos WHERE ID = ?', (id_produto,))
+        conn.commit()
+        limpar_campos()
+        atualizar_tabela()
 
-    if evento == 'Cadastrar':
-        nome = valores['nome']
-        marca = valores['marca']
-        preco = valores['preco']
-        quantidade = valores['quantidade']
-
-        if nome and marca and preco and quantidade:
-            cadastrar_produto(nome, marca, float(preco), int(quantidade))
-            sg.popup('Produto cadastrado com sucesso!')
-        else:
-            sg.popup('Preencha todos os campos.')
-
-    if evento == 'Editar':
-        produtos = listar_produtos()
-        if not produtos:
-            sg.popup('Nenhum produto cadastrado.')
-        else:
-            selected_product = valores['produtos']
-            if selected_product:
-                selected_product_id = selected_product[0].split('-')[0].strip()
-                nome = valores['nome']
-                marca = valores['marca']
-                preco = valores['preco']
-                quantidade = valores['quantidade']
-
-                if nome and marca and preco and quantidade:
-                    atualizar_produto(int(selected_product_id), nome, marca, float(preco), int(quantidade))
-                    sg.popup('Produto atualizado com sucesso!')
-                else:
-                    sg.popup('Preencha todos os campos.')
-
-    if evento == 'Excluir':
-        produtos = listar_produtos()
-        if not produtos:
-            sg.popup('Nenhum produto cadastrado.')
-        else:
-            selected_product = valores['produtos']
-            if selected_product:
-                selected_product_id = selected_product[0].split('-')[0].strip()
-                excluir_produto(int(selected_product_id))
-                sg.popup('Produto excluído com sucesso!')
-
-    # Atualizar a lista de produtos na interface
-    produtos = listar_produtos()
-    janela['produtos'].Update([f'{produto[0]} - {produto[1]} ({produto[2]} - {produto[3]})' for produto in produtos])
-
-# Fechar a janela
-janela.Close()
+window.close()
+conn.close()
